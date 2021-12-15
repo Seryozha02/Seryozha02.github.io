3470cf7903b418d40e4fbf024daf76d449382b57
Seryozha02 <seryozha.asatryan02@gmail.com>
2021-12-15 21:05:37 +0400

    Brief

---
+++ script.js
@@ -0,0 +1,334 @@
+let programmers =[ {
+  id: 1,
+  gender: "Male",
+  image: "./img/boy.png",
+  firstName: " Alex",
+  lastName: "Hambardzumyan ",
+  phone: " +37493114415",
+  email: "alexanhambarcumyan@gmail.com",
+  education:  "Shushi Technology university",
+  workExperience: "AIPC" ,
+  trainings: "Relq school",
+},
+{
+  id: 3,
+  gender: "female",
+  image: "./img/girl.png",
+  firstName: " Arusyak",
+  lastName: " Aghajanyan",
+  phone:" 055900155" ,
+  email: "aghajanyanarusyak@gmail.com",
+  education: "YSUGU",
+  workExperience: "InecoBank CJSC, Risk meneger",
+  trainings: "BasicItCenter,UNDP Armenia Accelerator5,HTI Armenia C++ basic, SmartCode,Relq Technology",
+},
+{
+  id: 3,
+  gender: "Female",
+  image: "./img/girl.png",
+  firstName: "Lusya",
+  lastName: " Petrosyan",
+  phone: "+37493322902",
+  email: "plusine99@gmail.com",
+  education: "Armenian State University faculty of Radiophysics",
+  workExperience: "",
+  trainings: "",
+},
+{
+  id: 4,
+  gender: "Male",
+  image: "./img/boy.png",
+  phone: "+37494276700",
+  firstName: "Narek",
+  lastName: "Tovmasyan",
+  email: "Narektom333@gmail.com",
+  workExperience: "Ameria bank",
+  education: "Armenian State University of Economics",
+  trainings: "Relq",
+  
+},
+{
+  id: 5,
+  gender: "Male",
+  image: "./img/boy.png",
+  phone: "+37477760986",
+  firstName: "Suren",
+  lastName: "Galstyan",
+  email: "suren.galstyan@gmail.com",
+  workExperience: "",
+  education: "undefined",
+
+},
+{
+  id: 6,
+  gender: "Female",
+  image: "./img/girl.png",
+  firstName: "Hasmik",
+  lastName: "Gevorgyan",
+  phone: "+37499463623",
+  email: "gevorgyan.icml@gmail.com",
+  education: "YEREVAN STATE COLLEGE OF INFORMATICS" ,
+  workExperience: "Mon Amie Flowers & Design boutique",
+  trainings: "Relq Technology",
+
+},
+ {
+  id: 7,
+  gender: "Male",
+  image: "./img/boy.png",
+  firstName: "Sevak",
+  lastName: "Stepanyan",
+  phone: "+37477121696",
+  email: "stepanyansevak9@gamil.com",
+  education: "Economics and Menegment",
+  workExperience: "2 Years",
+  trainings: "Relq Technology"
+},
+ {
+  id: 8,
+  gender: "Female",
+  image: "./img/girl.png",
+  firstName: "Larine",
+  lastName: "Danielyan",
+  phone: "+37497759599",
+  email: "danielyanlari@gmail.com",
+  education: "Economics and Menegment",
+  workExperience: "Martuninsky district Medical Association",
+  trainings: "Relq Technology",
+},
+ {
+  id: 9,
+  gender: "Male",
+  image: "./img/boy.png",
+  firstName: "Armenak",
+  lastName: "Qocharyan",
+  phone: "+37494902171",
+  email: "qocharyanarmenak@gmail.com",
+  education:"Bardzraguyn zinvorakan",
+  workExperience: "HH PN",
+  trainings: "Relq school",
+},
+ {
+  id: 10,
+  gender: "Male",
+  image: "./img/boy.png",
+  firstName: "Tigran ",
+  lastName: "Martirosyan",
+  phone:"+37477151868" ,
+  email: "martirosyant01@mail.ru",
+  education: "",
+  workExperience: "",
+  trainings: "Relq school",
+},
+ {
+  id: 11,
+  gender: "Male",
+  image: "./img/boy.png",
+  firstName: " Seryozha",
+  lastName: "Asatryan ",
+  phone: "+37494246880",
+  email: "seryozha.asatryan02@gmail.com",
+  education: "Economics" ,
+  workExperience: "" ,
+  trainings: "Relq school", 
+},
+ {
+  id: 12,
+  gender: "Male",
+  image: "./img/boy.png",
+  firstName: "Marat",
+  lastName: "Harutyunyan",
+  phone: "+37444055150",
+  email: "maratharutyunyan.mh.mh@gmail.com",
+  education: "ASIPCS" ,
+  workExperience: "EL Market",
+  trainings: "Relq Technology",
+},
+ {
+  id: 13,
+  gender: "Female",
+  image: "./img/girl.png",
+  firstName: "Lusine",
+  lastName: " Yeremyan",
+  phone: "+37443181834",
+  email: "lusineer@gmail.com",
+  education: "Armenian State University of Economics",
+  workExperience: "Yerevan Municipality",
+  trainings: "MIC",
+},
+{
+  id: 14,
+  gender: "Male",
+  image: "./img/boy.png",
+  phone: "+37493285595",
+  firstName: "Babken",
+  lastName: "Tadevosyan",
+  email: "babken.tadevosyan@gmail.com",
+  workExperience: "undefined",
+  education: "undefined",
+
+},
+ {
+  id: 15,
+  gender: "Female",
+  image: "./img/girl.png",
+  phone: "+37497336216",
+  firstName: "Hermine",
+  lastName: "Sahakyan",
+  email: "hermine.sahakyan@gmail.com",
+  workExperience: "undefined",
+  education: "undefined",
+  
+},
+{
+  id: 16,
+  gender: "Female",
+  image: "./img/girl.png",
+  phone: "+37443266599",
+  firstName: "Hermine",
+  lastName: "Melqumyan",
+  email: "hermine.melqumyan@gmail.com",
+  workExperience: "undefined",
+  education: "undefined",
+ 
+},
+{
+  id: 17,
+  gender: "Female",
+  image: "./img/girl.png",
+  phone: "+37496333328",
+  firstName: "Afina",
+  lastName: "Panyan",
+  email: "afina.panyan@gmail.com",
+  workExperience: "undefined",
+  education: "undefined",
+
+},
+ {
+  id: 18,
+  gender: "Female",
+  image: "./img/girl.png",
+  firstName: "Liana",
+  lastName: "Dashyan",
+  phone: "+37491555374",
+  email: "dashyanlian@gmail.com",
+  education: "YSU" ,
+  workExperience: "GYU NGO",
+  trainings: "Relq Technology",
+},
+{
+  id: 19,
+  gender: "Female",
+  image: "./img/girl.png",
+  firstName: "Anna",
+  lastName: "Hayriyan",
+  phone: "+37498144450",
+  email: "annhayriyan@gmail.com",
+  education: "ASUE" ,
+  workExperience: "Scott logistics",
+  trainings: "Relq Technology",
+},
+{
+  id: 20,
+  gender: "Female",
+  image: "./img/girl.png",
+  phone: "+37444148000",
+  firstName: "Anahit",
+  lastName: "Khechumyan",
+  email: "chumyan63@gmail.com ",
+  workExperience: "School Principal-School N67 ",
+  education: " Yerevan State University",
+  trainings: "Bitschool , RELQ",
+},
+{
+  id: 21,
+  gender: "Female",
+  image: "./img/girl.png",
+  phone: "+37441900802",
+  firstName: "Ani",
+  lastName: "Antonyan",
+  email: "ani.antonyan@gmail.com",
+  workExperience: "undefined",
+  education: "undefined",
+
+},
+{
+  id: 22,
+  gender: "Female",
+  image: "./img/girl.png",
+  firstName:"Seda",
+  lastName:"Shahnazaryan",
+  phone: "+37498313871",
+  email: "shahnazaryan.seda@mail.ru",
+  education: "Higher Education:Historian",
+  workExperince: "15 years",
+  trainings: "TechBoost4Women,RELQ technology school",
+},
+ {
+  id: 23,
+  gender: "Female",
+  image: "./img/girl.png",
+  phone: "+37498200272",
+  firstName: "Silva",
+  lastName: "Ohanyan",
+  email: "silva.ohanyan@gmail.com",
+  workExperience: "undefined",
+  education: "undefined",
+
+},
+]
+
+
+console.log(programmers);
+list();
+
+function list() {
+  let container = document.querySelector(".container");
+  programmers.forEach(function (programmer) {
+    container.innerHTML += `<div class="card">      
+        <img src="${programmer.image}" alt="person-icon"> 
+        <div class = "bio">
+         ${programmer.firstName} ${programmer.lastName}
+         </div>
+          <div class="btn">
+          <a href="#bioPage?=${programmer.id}" class="biolink"><button>Read More</button></a>
+          </div>
+          
+          </div>`;
+          
+  });
+}
+
+window.addEventListener("load", () => {
+  console.log(window.location);
+});
+
+window.addEventListener("load", function () {
+  console.log("LOAD eventlistener called");
+});
+window.addEventListener("hashchange", function () {
+  let container = document.querySelector(".container");
+  let personalPage = document.querySelector(".box");
+  if (window.location.hash.includes("bioPage")) {
+    container.classList.add("hidden");
+    personalPage.classList.remove("hidden");
+    //vercnel hash id , programmers[id]
+    let hash = window.location.hash.split("=");
+    let id = hash[1]; // popokhel
+    console.log ("id = ", id);
+    addUserData(programmers[id - 1]);
+  } else {
+    container.classList.remove("hidden");
+    personalPage.classList.add("hidden");
+  }
+});
+function addUserData(programmer) {
+  console.log("personalData===", programmer);
+  document.querySelector(".cvName").innerHTML = programmer.firstName + " " + programmer.lastName;
+  document.querySelector(".email").innerHTML = programmer.email;
+  document.querySelector(".education").innerHTML = programmer.education === "undefined" ? "notProvited": programmer.education ;
+  document.querySelector(".workExperience").innerHTML = programmer.workExperience === "undefined" ? "notProvited": programmer.workExperience;
+  document.querySelector(".phone").innerHTML = programmer.phone;
+  document.querySelector(".image img").src = programmer.gender === "Male" ? "./img/boy.png" : "./img/girl.png";
+  document.querySelector("#trainings").innerHTML = programmer.trainings;
+}
